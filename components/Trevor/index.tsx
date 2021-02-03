43e7f0c5187fadd3e2b057405ef9dde8fe132767
Trevor <mangrumtrevor@gmail.com>
2021-02-03 17:20:43 -0600

    Implements components necessary for example issue

---
+++ components/Trevor/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./trevor.module.scss";
+
+const Trevor: React.FC = () => {
+  return <h1 className={styles["trevor"]}>Trevor</h1>;
+};
+
+export default Trevor;
--- tests/examples.test.ts
+++ tests/examples.test.ts
@@ -1,5 +1,6 @@
 import { sum } from "server/sum";
-
+import { mult } from "server/product";
+import fetch from "isomorphic-unfetch";
 //Put the name of your function in the first parameter of describe.
 describe("sum", () => {
   //the it() function will contain your actual test. Use the first parameter of it to describe what your test is testing for.
@@ -10,4 +11,27 @@ describe("sum", () => {
   });
 });
 
-//TODO: An asynchronous example
+describe("mult", () => {
+  it("Make sure that mult returns the correct calculations", () => {
+    expect(mult(2, 2)).toBe(4);
+    expect(mult(2, 6)).toBe(12);
+    expect(mult(4, 4)).not.toBe(20);
+  });
+});
+
+describe("API Route", () => {
+  it("Makes sure name is returned", async () => {
+    const response = await fetch("http://localhost:3000/api/cart", {
+      method: "GET",
+    });
+
+    const data = (await response.json()) as {
+      success: boolean;
+      payload: string;
+    };
+
+    expect((data as { success: boolean; payload: string }).payload).toBe(
+      "Trevor"
+    );
+  });
+});
