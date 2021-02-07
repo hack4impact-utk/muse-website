ce72c6ea2fc9d043dcad853b5a5bd59c3aed3aea
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-02-06 19:42:59 -0600

    Implements global stylesheet and adds sass vars

--- components/Trevor/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./trevor.module.scss";
-
-const Trevor: React.FC = () => {
-  return <h1 className={styles["trevor"]}>Trevor</h1>;
-};
-
-export default Trevor;
