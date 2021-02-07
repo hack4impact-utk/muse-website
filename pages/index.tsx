078e71380bddc7856d4738186c05bb9b0a27f5bf
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-02-06 19:44:35 -0600

    Removes Trevor component from home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from "next";
-import Trevor from "components/Trevor";
 const Home: NextPage = () => {
   return (
     <main>
-      <Trevor />
       <h1>This is our home page!</h1>
     </main>
   );
