e9c9ff8e9f8f43d8662d5ee2b0baebccf16b1cdc
Trevor Mangrum <25257772+trevormangrum@users.noreply.github.com>
2021-02-07 12:04:26 -0600

    Sets up starter SASS variables and global stylesheet (#8)
    * Implements global stylesheet and adds sass vars
    
    * Removes Trevor component from home
    
    * removes API route test
    
    Co-authored-by: Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import type { AppProps } from "next/app";
+import "styles/globals.scss";
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
