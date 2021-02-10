06b75ce59a5445c899afaf4359bd93c921d122bf
jovanyoshioka <jovanyoshioka@gmail.com>
2021-02-10 17:32:18 -0500

    Framework of Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,2 +1,9 @@
-// TODO Implement this commponent.
-export {};
+import styles from "./footer.module.scss";
+
+const Footer: React.FC = () => {
+  return (
+    <h2 className={styles.footer}>Footer Here</h2>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import { NextPage } from "next";
+import Footer from "components/Footer";
+
 const Home: NextPage = () => {
   return (
     <main>
       <h1>This is our home page!</h1>
+
+      <Footer />
     </main>
   );
 };
