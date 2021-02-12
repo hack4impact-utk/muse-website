993f53e2448920d7d4600092d7e7c56f92efc3c1
Jacob <armigersemail@gmail.com>
2021-02-11 20:27:03 -0500

    Jacob armiger/muse 24 connect connectful api (#10)
    * Adds contentful-management package
    
    * Creates Contentful client

--- server/actions/Contentful/index.tsx
+++ server/actions/Contentful/index.tsx
@@ -1,3 +1,8 @@
+import { createClient } from "contentful-management";
+const client = createClient({
+  accessToken: process.env.REACT_APP_DELIVERY_KEY as string,
+});
+
 //TODO Delete this export and fill out this file with functions relating to the Contentful API.
 //Be sure to export major functions by using the "export" keyword before your function definition.
 //Ex: export async function <function_name> () {}
