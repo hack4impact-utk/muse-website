496b8a7b85e98a29ee26fbe83bd0dc070d834285
Ray Luberto <rluberto15@gmail.com>
2021-02-22 17:41:56 -0500

    Header Component (#12)
    * header draft
    
    * update style
    
    Co-authored-by: = <=>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import Header from "components/Header";
+        
 const Home: NextPage = () => {
   return (
     <main>
+      <Head>
+        <title>Muse Knoxville</title>
+      </Head>
+      <Header />
       <h1>This is our home page!</h1>
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Arial;
+        }
+      `}</style>
     </main>
   );
 };
