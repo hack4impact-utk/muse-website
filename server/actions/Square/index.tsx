5c6b07274ba5efff72643e42c7f467b3e8a4baa8
jacob-armiger <armigersemail@gmail.com>
2021-02-25 11:46:06 -0500

    Connects Square API and implements access token refresh

--- server/actions/Square/index.tsx
+++ server/actions/Square/index.tsx
@@ -1,8 +1,39 @@
 import { Client, Environment } from "square";
+import { preProcessFile } from "typescript";
 
+// Use our refresh token to get the current access token
+export async function getAccessToken(): Promise<string> {
+  let token;
+
+  try {
+    const response = await client.oAuthApi.obtainToken({
+      clientId: process.env.CLIENT_ID as string,
+      clientSecret: process.env.CLIENT_SECRET as string,
+      grantType: "refresh_token",
+      refreshToken: process.env.REFRESH_TOKEN as string,
+      shortLived: false,
+    });
+
+    // Set new token
+    token = response.result.accessToken;
+
+    //console.log(response.httpResponse);
+  } catch (error) {
+    console.log(error);
+  }
+  return token as string;
+}
+
+// Create Square client
 const client = new Client({
   environment: Environment.Sandbox,
-  accessToken: process.env.SQUARE_ACCESS_TOKEN,
 });
 
-export {};
+// Example of using .withconfiguration to create a new instance of the client
+// with current access token
+export async function getItemsByCategory(category: string): Promise<void> {
+  const token = await getAccessToken();
+  const newClient = client.withConfiguration({
+    accessToken: token,
+  });
+}
