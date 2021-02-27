0533d594e0d96a1fd71f27b4cf4616ed5d47ac9a
jovanyoshioka <jovanyoshioka@gmail.com>
2021-02-27 12:23:32 -0500

    Resolved linter issues.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
     <footer className={styles.container}>
       <div className={styles.row}>
         <div className={`${styles.column} ${styles.map}`}>
-          <iframe src={mapSrc}></iframe>
+          <iframe title="Muse Location" src={mapSrc}></iframe>
         </div>
         <div className={`${styles.column} ${styles.information}`}>
           <h1>CONTACT INFORMATION</h1>
@@ -28,60 +28,60 @@ const Footer: React.FC = () => {
           <p>Saturday - 10am-12pm, 1-3pm, 3:30-5:30pm</p>
           <p>Sunday - 10am-12pm, 1-3pm</p>
           <div className={styles.smContainer}>
-            <a href="https://www.facebook.com/MuseKnoxville/" target="_blank"><button><FaFacebookF /></button></a>
-            <a href="https://www.instagram.com/museknox/" target="_blank"><button><FaInstagram /></button></a>
-            <a href="https://twitter.com/MuseKnox" target="_blank"><button><FaTwitter /></button></a>
-            <a href="https://www.youtube.com/channel/UC4b-RIBm744vaDDlWX55pyQ" target="_blank"><button><FaYoutube /></button></a>
+            <a href="https://www.facebook.com/MuseKnoxville/" rel="noreferrer" target="_blank"><button><FaFacebookF /></button></a>
+            <a href="https://www.instagram.com/museknox/" rel="noreferrer" target="_blank"><button><FaInstagram /></button></a>
+            <a href="https://twitter.com/MuseKnox" rel="noreferrer" target="_blank"><button><FaTwitter /></button></a>
+            <a href="https://www.youtube.com/channel/UC4b-RIBm744vaDDlWX55pyQ" rel="noreferrer" target="_blank"><button><FaYoutube /></button></a>
           </div>
         </div>
       </div>
       <div className={styles.row}>
         <div className={`${styles.column} ${styles.links}`}>
           <h2>Visit</h2>
-          <a>Plan Your Visit</a>
-          <a>Reopening Info</a>
-          <a>Memberships</a>
-          <a>Visitor FAQs</a>
-          <a>Field Trips</a>
-          <a>Free Family Night</a>
-          <a>Link</a>
+          <a href="#">Plan Your Visit</a>
+          <a href="#">Reopening Info</a>
+          <a href="#">Memberships</a>
+          <a href="#">Visitor FAQs</a>
+          <a href="#">Field Trips</a>
+          <a href="#">Free Family Night</a>
+          <a href="#">Link</a>
         </div>
         <div className={`${styles.column} ${styles.links}`}>
           <h2>Event Bookings</h2>
-          <a>Birthdays</a>
-          <a>Museum Rentals</a>
+          <a href="#">Birthdays</a>
+          <a href="#">Museum Rentals</a>
         </div>
         <div className={`${styles.column} ${styles.links}`}>
           <h2>Educators</h2>
-          <a>Field Trips</a>
-          <a>Outreach (In-School)</a>
-          <a>Preschool</a>
-          <a>Homeschool</a>
-          <a>Family STEAM Events</a>
-          <a>Community Events</a>
-          <a>Mobile Planetarium</a>
+          <a href="#">Field Trips</a>
+          <a href="#">Outreach (In-School)</a>
+          <a href="#">Preschool</a>
+          <a href="#">Homeschool</a>
+          <a href="#">Family STEAM Events</a>
+          <a href="#">Community Events</a>
+          <a href="#">Mobile Planetarium</a>
         </div>
         <div className={`${styles.column} ${styles.links}`}>
           <h2>Experiences</h2>
-          <a>Exhibits and Play Spaces</a>
-          <a>Muse at Home</a>
-          <a>Amusement Kits</a>
-          <a>On-Site Programs</a>
-          <a><span>Learning Pods</span></a>
+          <a href="#">Exhibits and Play Spaces</a>
+          <a href="#">Muse at Home</a>
+          <a href="#">Amusement Kits</a>
+          <a href="#">On-Site Programs</a>
+          <a href="#"><span>Learning Pods</span></a>
         </div>
         <div className={`${styles.column} ${styles.links}`}>
           <h2>About Us</h2>
-          <a>Mission & History</a>
-          <a>Leadership</a>
-          <a>Muse eNewsletter</a>
-          <a>Press</a>
-          <a>Contact Us</a>
+          <a href="#">Mission & History</a>
+          <a href="#">Leadership</a>
+          <a href="#">Muse eNewsletter</a>
+          <a href="#">Press</a>
+          <a href="#">Contact Us</a>
         </div>
         <div className={`${styles.column} ${styles.links}`}>
           <h2>Join+Support</h2>
-          <a>Donate Now</a>
-          <a>Volunteer</a>
-          <a>Muse for All Scholarships</a>
+          <a href="#">Donate Now</a>
+          <a href="#">Volunteer</a>
+          <a href="#">Muse for All Scholarships</a>
         </div>
       </div>
     </footer>
