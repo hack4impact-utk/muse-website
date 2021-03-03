26d06f2063615758e844fc3158b23297a6b53ec2
jovanyoshioka <jovanyoshioka@gmail.com>
2021-03-03 00:38:26 -0500

    Fix for header hiding page content. Added shop page file.

---
+++ pages/shop/index.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+const Shop: NextPage = () => {
+  return (
+    <main>
+      <Header />
+      
+      <h1>Shop</h1>
+      
+      <Footer />
+
+      <style jsx>{`
+        
+      `}</style>
+    </main>
+  );
+}
+
+export default Shop;
