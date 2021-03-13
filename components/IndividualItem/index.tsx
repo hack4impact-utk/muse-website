26b53acfa849391b1547ad27a77fc8119354c477
Nolan Askew <nolanaskew@gmail.com>
2021-03-12 21:09:47 -0500

    Created individual item component

---
+++ components/IndividualItem/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./individualItem.module.scss";
+
+const IndividualItem: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.image}>
+        <img src={"/item.png"} alt="Core value 1"></img>
+      </div>
+
+      <div className={styles.body}>
+        <h1>Full Day Pass for Muse Knoxville Week 9-12</h1>
+        <div className={styles.description}>
+          Description of item in detail if it is needed here. At the moment it
+          is filler text but this is the place customers can see what comes
+          included with the tickets or learning kits they are purchasing.
+        </div>
+        <button className={styles.button}>Add to Cart</button>
+        <h1>$30.00</h1>
+        <div className={styles.quantity}>
+          QTY <input type="text"></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IndividualItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+<<<<<<< HEAD
 import Layout from "components/Layout";
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,15 @@ const Home: NextPage = () => {
       <h1>This is our home page!</h1>
     </Layout>
   );
+=======
+import Header from "components/Header";
+import OurMission from "../components/OurMission/index";
+import Partners from "../components/Partners/index";
+import IndividualItem from "../components/IndividualItem/index";
+
+const Home: NextPage = () => {
+  return <IndividualItem />;
+>>>>>>> 3151971... Created individual item component
 };
 
 export default Home;
