f045494fac83ecdbe6653903371ed35d8c964fa1
Ray Luberto <rluberto15@gmail.com>
2021-03-26 18:31:12 -0400

    initial view container (#18)
    Co-authored-by: = <=>

---
+++ components/Home/InitialView/index.tsx
@@ -0,0 +1,31 @@
+import React from "React";
+import style from "./initialview.module.scss";
+
+const InitialView: React.FC = () => {
+  return (
+    <div className={style.parent}>
+      <div className={style.textParent}>
+        <h1>
+          We Inspire &<br></br>Empower
+        </h1>
+        <div className={style.ctaButtonWrapper}>
+          <a href="membership" className={style.ctaButton}>
+            Memberships
+          </a>
+        </div>
+      </div>
+      <img
+        className={style.gearGreen}
+        src="gear-green.png"
+        alt="Green gear for the Muse Knoxville."
+      ></img>
+      <img
+        className={style.gearYellow}
+        src="gear-yellow.png"
+        alt="Yellow gear for the Muse Knoxville."
+      ></img>
+    </div>
+  );
+};
+
+export default InitialView;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
 import { NextPage } from "next";
 import Head from "next/head";
+
+import Header from "components/Header";
+import InitialView from "components/Home/InitialView";
 import Layout from "components/Layout";
+        
 const Home: NextPage = () => {
   return (
     <Layout>
       <Head>
         <title>Muse Knoxville</title>
       </Head>
-      <h1>This is our home page!</h1>
+      <Header />
+      <InitialView />
+      <Footer />
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Arial;
+        }
+      `}</style>
     </Layout>
   );
 };
