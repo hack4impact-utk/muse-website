15ca7b4042cfbc87054e3e2f5839e8ffaea6fe77
Ray Luberto <rluberto15@gmail.com>
2021-03-31 11:22:47 -0400

    Fix Issue with Initial View Merge Conflicts (#26)
    * fix conflicts
    
    * fix react import warning
    
    Co-authored-by: = <=>

--- components/Home/InitialView/index.tsx
+++ components/Home/InitialView/index.tsx
@@ -1,4 +1,4 @@
-import React from "React";
+import React from "react";
 import style from "./initialview.module.scss";
 
 const InitialView: React.FC = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,14 @@ import Head from "next/head";
 import Header from "components/Header";
 import InitialView from "components/Home/InitialView";
 import Layout from "components/Layout";
-        
+
 const Home: NextPage = () => {
   return (
     <Layout>
       <Head>
         <title>Muse Knoxville</title>
       </Head>
-      <Header />
       <InitialView />
-      <Footer />
       <style jsx global>{`
         body {
           margin: 0;
