63add8e59ebf7b7afcddfeffb5047e19489da970
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-04-29 10:16:24 -0500

    Adds custom button component to builder

--- pages/[...page].tsx
+++ pages/[...page].tsx
@@ -6,20 +6,21 @@ import {
 } from "@builder.io/react";
 import { GetStaticPropsContext } from "next";
 import Layout from "components/Layout";
+import "components/Builder/Button";
 //All of this code below comes from the Builder.io GitHub
 //https://github.com/BuilderIO/builder/blob/master/examples/next-js-builder-site/src/pages/%5B...page%5D.tsx
 builder.init(process.env.NEXT_PUBLIC_BUILDER_IO_KEY as string);
 const USE_CODEGEN = false;
 interface Props {
-    builderPage: BuilderContent;
-};
-
-
+  builderPage: BuilderContent;
+}
 
 const MyComponent: React.FC<Props> = ({ builderPage }) => {
-  const heroText = builderPage ? `${(builderPage.data.pageTitle || builderPage.data.title)}` : "Muse Knoxville";
+  const heroText = builderPage
+    ? `${builderPage.data.pageTitle || builderPage.data.title}`
+    : "Muse Knoxville";
   return (
-    <Layout options={{hero: true, heroSize: "md", heroText: heroText} }>
+    <Layout options={{ hero: true, heroSize: "md", heroText: heroText }}>
       {builderPage || Builder.isEditing || Builder.isPreviewing ? (
         <BuilderComponent
           content={builderPage}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import InitialView from "components/Home/InitialView";
+import OurMission from "components/OurMission";
 import Layout from "components/Layout";
 
 const Home: NextPage = () => {
   return (
-    <Layout>
+    <Layout options={{initialView: true}}>
       <Head>
         <title>Muse Knoxville</title>
       </Head>
-      <InitialView />
+      <OurMission/>
       <style jsx global>{`
         body {
           margin: 0;
