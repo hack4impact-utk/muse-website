ffb3ead9f363d142973b30c4f6b77a1419a8caad
omarsitoe <55901450+omarsitoe@users.noreply.github.com>
2021-06-16 19:48:03 -0400

    created function that gets seatblocks from Bookeo (#39)

--- server/actions/Bookeo/index.tsx
+++ server/actions/Bookeo/index.tsx
@@ -7,19 +7,20 @@ export default class Bookeo_Client {
   apiKey: string;
   secretKey: string;
 
+  permissions: string;
+
   constructor() {
     this.baseUrl = baseURL;
     this.apiKey = process.env.BOOKEO_API_KEY as string;
     this.secretKey = process.env.BOOKEO_APP_SECRET as string;
+
+    this.permissions = `apiKey=${this.apiKey}` + `&secretKey=${this.secretKey}`;
   }
 
   // Simple function used to check validity of keys
   async check_keys(): Promise<string> {
     //let finishedRequestBody = "";
-    const uri =
-      `${this.baseUrl}settings/apikeyinfo?` +
-      `apiKey=${this.apiKey}` +
-      `&secretKey=${this.secretKey}`;
+    const uri = `${this.baseUrl}settings/apikeyinfo?${this.permissions}`;
 
     // Check api key
     console.log("Checking permissions...");
@@ -36,5 +37,48 @@ export default class Bookeo_Client {
 
   booking(): void {
     //TODO: implement booking function
+  }
+
+  async getProducts(): Promise<JSON> {
+    const uri = `${this.baseUrl}settings/products?${this.permissions}`;
+
+    console.log("Checking available products...");
+    const response = await fetch(uri);
+    const data: JSON = (await response.json()) as JSON;
+
+    return data;
+  }
+
+  async getSeatBlocks(
+    startTime?: string,
+    endTime?: string,
+    lastUpdatedStartTime?: string,
+    lastUpdatedEndTime?: string,
+    productId?: string,
+    itemsPerPage?: string,
+    pageNavigationToken?: string,
+    pageNumber?: string
+  ): Promise<JSON> {
+    let uri = `${this.baseUrl}/seatblocks?${this.permissions}`;
+
+    // Build uri with parameters
+    if (startTime != null) uri += `startTime=${startTime}&`;
+    if (endTime != null) uri += `endTime=${endTime}&`;
+    if (lastUpdatedStartTime != null)
+      uri += `lastUpdatedStartTime=${lastUpdatedStartTime}&`;
+    if (lastUpdatedEndTime != null)
+      uri += `lastUpdatedEndTime=${lastUpdatedEndTime}&`;
+
+    if (productId != null) uri += `productId=${productId}&`;
+    if (itemsPerPage != null) uri += `itemsPerPage=${itemsPerPage}&`;
+    if (pageNavigationToken != null)
+      uri += `pageNavigationToken=${pageNavigationToken}&`;
+    if (pageNumber != null) uri += `pageNumber=${pageNumber}&`;
+
+    // Send request with specified parameters
+    const response = await fetch(uri);
+    const data: JSON = (await response.json()) as JSON;
+
+    return data;
   }
 }
