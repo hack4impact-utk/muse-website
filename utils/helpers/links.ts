0001e58db84ae40cad7419b320076c698c370f05
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-06-18 13:20:38 -0500

    Attmempts to fix linter errors

--- utils/helpers/links.ts
+++ utils/helpers/links.ts
@@ -1,19 +1,18 @@
-import { builder } from "@builder.io/react";
+import { builder, BuilderContent } from "@builder.io/react";
 builder.init(process.env.NEXT_PUBLIC_BUILDER_IO_KEY as string);
 /**
  * Gets information about every page that is made in Builder.io. This is used to sort the pages into different categories.
  * @returns An array of builder.io data.
  */
-export const getDropdownLinks = async (): Promise<unknown[]> => {
+export const getDropdownLinks = async (): Promise<any[]> => {
   try {
-    let results = await builder.getAll("page", {
+    const results = await builder.getAll("page", {
       key: "pages:all",
       fields: "data",
       options: {
         noTargeting: true,
       },
     });
-    console.log("Results:", results);
     return results;
   } catch (error) {
     return [];
