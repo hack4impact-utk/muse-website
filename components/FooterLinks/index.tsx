985db4913d1c46e93c8619b19482001ec066dcc6
Trevor Mangrum <25257772+trevormangrum@users.noreply.github.com>
2021-07-21 22:03:57 -0500

    Dynamically loads footer links based on builder.io (#51)
    Co-authored-by: Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>

---
+++ components/FooterLinks/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import useSWR from "swr";
+import urls from "utils/urls";
+import styles from "components/Footer/footer.module.scss";
+const FooterLinks: React.FC =  () => {
+    const fetcher = (url:string): Promise<unknown> => fetch(url).then(r => r.json());
+    const {data, error} = useSWR(`${urls.baseUrl}${urls.api.dropdowns}`, fetcher);
+    console.log(data);
+  //https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript 
+    const dropdownNames = data && [...new Set(data.payload.map(item => item.data.dropdownCategory))];
+    return (
+    <div className={styles.row}>
+    {data && dropdownNames && (
+        dropdownNames.map(name => {
+            return (
+                <div className={`${styles.column} ${styles.links}`}>
+                    <h2 className={styles.navButtonMain}>{name}</h2>
+                    {data && (
+                        data.payload.map(item => {
+                            if(item.data.dropdownCategory === name) {
+                                return <a key={item.data.title} href={item.data.url} >{item.data.title}</a>
+                            }
+                            return <></>
+                        })
+                    )}
+                
+                </div>
+            )
+        })
+    )}
+    </div>
+   );
+}
+
+export default FooterLinks;
